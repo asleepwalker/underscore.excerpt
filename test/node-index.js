c899886bd5a092c308b03dafe704ee80089a8400
Artyom 'Sleepwalker' Fedosov <mail@asleepwalker.ru>
2017-01-03 14:32:33 +0200

    Update years in license and copyright banners

--- test/node-index.js
+++ test/node-index.js
@@ -1,3 +1,9 @@
+/*
+*	Underscore.Excerpt, v1.0.2
+*	(c) 2014–2017 Artyom "Sleepwalker" Fedosov <mail@asleepwalker.ru>
+*	https://github.com/asleepwalker/underscore.excerpt
+*/
+
 var qunit = require('qunit');
 
 qunit.run({
